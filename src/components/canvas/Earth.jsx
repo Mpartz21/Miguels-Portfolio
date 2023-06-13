a253ad50e290fd39e5bc58d6cea25af7b3895c43
Mpartz21 <mig21.ap@gmail.com>
2023-06-13 01:10:21 -0700

    taking out the earth

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -4,19 +4,19 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
 import CanvasLoader from "../Loader"
 
 
-const Earth = () => {
+// const Earth = () => {
 
-  const earth = useGLTF('/planet/scene.gltf', true);
+//   const earth = useGLTF('/planet/scene.gltf', true);
 
-  return (
-    <primitive 
-    object={earth.scene} 
-    scale={2.2}
-    position-y={0}
-    rotation-y={0}
-    />
-  )
-}
+//   return (
+//     <primitive 
+//     object={earth.scene} 
+//     scale={2.2}
+//     position-y={0}
+//     rotation-y={0}
+//     />
+//   )
+// }
 
 const EarthCanvas = () => {
   return (
@@ -37,7 +37,7 @@ const EarthCanvas = () => {
         maxPolarAngle={Math.PI / 2}
         minPolarAngle={Math.PI / 2}
       />
-      <Earth/>  
+      {/* <Earth/>   */}
     </Canvas>
   )
 }

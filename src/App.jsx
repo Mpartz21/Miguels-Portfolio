4b86a8fa3e3a1c115c6608cb560513726c489bc5
Mpartz21 <mig21.ap@gmail.com>
2023-06-13 17:04:49 -0700

    update header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom"
 import { About, Contact, Experience, Feedbacks,
-         Navbar, Tech, Works} from "./components"
+         Navbar, Tech, Works, Hero} from "./components"
 
 
 const App = () => {
@@ -9,7 +9,7 @@ const App = () => {
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-center bg-no-repeat bg-center">
           <Navbar />
-          {/* <Hero /> */}
+           <Hero /> 
         </div>
         <About />
         <Experience />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,9 @@ import SectionWrapper from '../hoc/SectionWrapper'
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto">
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
-        <div className="flex flex-col justify-center items-center mt-5">
+    <section className="relative w-full mx-auto">
+      <div className={`${styles.paddingX} absolute inset-0 max-w-7xl mx-auto flex flex-row items-start`}>
+        <div className="flex flex-col justify-center items-center">
           <div className="w-5 h-5 rounded-full bg-[#915eff]"/>
           <div className="w-1 sm:h-80 h-40 violet-gradient"/>
         </div>
@@ -37,4 +37,4 @@ const Hero = () => {
   )
 }
 
-export default SectionWrapper(Hero, "hero")
+export default SectionWrapper(Hero, "")

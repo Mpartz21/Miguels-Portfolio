134a81a1f9ccd57b651003c2e0662b696bc73700
Mpartz21 <mig21.ap@gmail.com>
2023-06-21 11:54:07 -0700

    Taking out Hero

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
-import Hero from './Hero';
 import Navbar from './Navbar';
 import About from './About';
 import Tech from './Tech';
@@ -10,7 +9,7 @@ import Contact from './Contact';
 import IntroTop from './Intro';
 
 export {
-  Hero,
+
   Navbar,
   About,
   Tech,

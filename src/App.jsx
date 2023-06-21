e3119905713dc4a058357b21cf00d364efeaeae2
Mpartz21 <mig21.ap@gmail.com>
2023-06-21 11:56:01 -0700

    hero taken out

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom"
 import  { IntroTop, About, Contact, Experience, Feedbacks,
-         Navbar, Tech, Works, Hero} from "./components"
+         Navbar, Tech, Works} from "./components"
 
 
 const App = () => {
@@ -10,7 +10,7 @@ const App = () => {
         <div className="bg-hero-pattern bg-cover bg-center bg-no-repeat bg-center">
           <Navbar />
           <IntroTop/>
-          <Hero /> 
+  
         </div>
         <About />
         <Experience />
